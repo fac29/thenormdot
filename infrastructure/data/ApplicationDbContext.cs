8c27a95e5dc4fa37a189dddafc8cc64a59800ed9
thatGui <thatguifonseca@gmail.com>
2024-07-24 12:48:29 +0100

    added the appdbcontext + neon optionsBuilder

--- infrastructure/data/ApplicationDbContext.cs
+++ infrastructure/data/ApplicationDbContext.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using dotenv.net;
+using Microsoft.EntityFrameworkCore;
 
 namespace infrastructure.data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            DotEnv.Load();
+            optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL"));
+        }
+    }
 
 }
