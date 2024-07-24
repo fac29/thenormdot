31970ea9e10bda9011e5ccdd753a3497b06643db
thatGui <thatguifonseca@gmail.com>
2024-07-24 17:08:21 +0100

    nothing changed

--- infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
+++ infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0ace82f70c2c08177a90bd192e52c24eb53e52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2a3142ce97e121b5e48a32c360097d5f07c80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
